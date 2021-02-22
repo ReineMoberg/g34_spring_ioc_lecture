288a1b19aa0da64bf46b6f1811ee2ead262f9c2b
mehrdad-javan <mehrdad.javan@adapta.se>
2021-02-22 10:32:50 +0100

    config spring container using xml define a bean in xml file load xml file

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,13 +1,26 @@
 package se.lexicon;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import se.lexicon.dao.AppUserDao;
+import se.lexicon.dao.AppUserDaoImpl;
+import se.lexicon.model.AppUser;
+
+public class App {
+    public static void main(String[] args) {
+
+        // load xml file
+        ClassPathXmlApplicationContext context= new ClassPathXmlApplicationContext("applicationContext.xml");
+        AppUserDao dao = context.getBean("appUserDao",AppUserDao.class);
+
+        AppUser createdAppUser1 = dao.saveOrUpdate(new AppUser("Test","test.test@test.se","123456","Vaxjo"));
+        System.out.println("createdAppUser = " + createdAppUser1);
+        AppUser createdAppUser2 = dao.saveOrUpdate(new AppUser("Mehrdad","mehrdad.javan@lexicon.se","123456","Vaxjo"));
+        System.out.println("createdAppUser = " + createdAppUser2);
+        System.out.println("-------------------------");
+        dao.findAll().forEach(System.out::println);
+        System.out.println("-------------------------");
+
+
     }
 }
